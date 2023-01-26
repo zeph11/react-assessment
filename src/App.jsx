5d75bec1823a50c0e580f176f23e940ac52d9634
rubek <rubek@ottr.com.np>
2023-01-26 15:24:15 +0545

    Configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
+    <div className="App container">
+      <div className="flex">
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="v__logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="v__logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="v__card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -24,7 +24,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="v__read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
