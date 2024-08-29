6df46e2e35674d9defc11eb81c93efd341a76ef0
zeph111 <luniva121@gmail.com>
2024-08-29 09:51:29 +0545

    task1:hidden message

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import "./App.css";
 import Child from "./components/Child";
 
 const App = () => {
-  const [value, setValue] = useState(-5);
+  const [value, setValue] = useState(0);
   // const [display, setDisplay] = useState(false);
 
   return (
     <div className="App">
       <h1>{value}</h1>
       <Child value={value} setValue={setValue} />
-      {value < 0 ? <p>Why so negative?</p> : <></>}
+      {value < 0 ? (
+        <p>Why so negative?</p>
+      ) : (
+        <p className="display_text">Hidden Text</p>
+      )}
     </div>
   );
 };
